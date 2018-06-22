13708edc918f5d6c40eca044b2a48d9a9a3eacc7
tamaraortizr <39891344+tamaraortizr@users.noreply.github.com>
2018-06-22 10:37:35 -0500

    Update minmax.py

--- minmax.py
+++ minmax.py
@@ -1,23 +1,27 @@
 infinito = 100000
 
-class Minmax(objeto):
-    def inicio(self, heuristic_eval):
+
+class Minimax(objeto):
+
+
+    def __init__(self, heuristic_eval):
         self.heuristic_eval = heuristic_eval
 
-    def minmax(self, board, masterboard, profundidad, jugador, oponente, a = -infinito, b = infinito ):
-        bestChild = board
+
+    def minimax(self, tablero, tableromaster, profundidad, jugador, oponente,
+                alfa = -infinito, beta = infinito):
+        bestChild = tablero
         if profundidad == 0:
-            return (self.heuristic_eval(masterboard. board, profundidad, jugador, oponente), board)
-        for n in board.next_states(jugador):
-            puntaje, n = self.minmax()
-        for child in board.next_states(player):
-            score, newChild = self.minimax(
-                child, board, depth - 1, opponent, player, -beta, -alfa)
-            score = -score
-            if score > alfa:
-                alfa = score
+            return (self.heuristic_eval(tablero, tablero, profundidad,
+                                        jugador, oponente), tablero)
+        for child in tablero.next_states(jugador):
+            puntuacion, newChild = self.minimax(
+                child, tablero, profundidad - 1, oponente, jugador, -beta, -alfa)
+            puntuacion = -puntuacion
+            if puntuacion > alfa:
+                alfa = puntuacion
                 bestChild = child
             if beta <= alfa:
                 break
-        return (self.heuristic_eval(board, board, depth, player,
-                                    opponent), bestChild)
+        return (self.heuristic_eval(tablero, tablero, profundidad, jugador,
+                                    oponente), bestChild)
