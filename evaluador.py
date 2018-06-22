2ffc3f87a8b91cfe39c58d14d1b010f2edfdfbac
tamaraortizr <39891344+tamaraortizr@users.noreply.github.com>
2018-06-22 13:09:15 -0500

    Update evaluador.py

--- evaluador.py
+++ evaluador.py
@@ -2,27 +2,27 @@ from variables import blanco, azul, vacio
 
 class Evaluador(object):
     perdertodo_puntuacion = 1000
-    pieza_contador_peso = [0, 0, 0, 4, 1]
-    posibles_peso = [5, 4, 3, 2, 0]
-    movimientos_peso = [7, 6, 5, 4, 0]
-    esquinas_peso = [35, 35, 35, 35, 0]
-    lados_peso = [0, 3, 4, 5, 0]
-    x_peso = [-8, -8, -8, -8, 0]
+    pieza_contador_valor = [0, 0, 0, 4, 1]
+    posibles_valor = [5, 4, 3, 2, 0]
+    movimientos_valor = [7, 6, 5, 4, 0]
+    esquinas_valor = [35, 35, 35, 35, 0]
+    lados_valor = [0, 3, 4, 5, 0]
+    x_valor = [-8, -8, -8, -8, 0]
 
     def diferencial_piezas(self, dtablero, banda):
-        if Evaluador.pieza_contador_peso[banda] != 0:
-            whites, blacks, empty = dtablero.contarpiezas()
+        if Evaluador.pieza_contador_valor[banda] != 0:
+            blancos, azules, vacios = dtablero.contarpiezas()
             if self.jugador == blanco:
                 mipuntuacion = blancos
                 tupuntuacion = azules
             else:
                 mipuntuacion = azules
                 tupuntuacion = blancos
-            return Evaluador.pieza_contador_peso[banda] * (mipuntuacion - tupuntuacion)
+            return Evaluador.pieza_contador_valor[banda] * (mipuntuacion - tupuntuacion)
         return 0
 
     def diferencial_esquinas(self, dcontar, dtablero, banda):
-        if Evaluador.esquinas_peso[banda] != 0:
+        if Evaluador.esquinas_valor[banda] != 0:
             mipuntuacion = 0
             tupuntuacion = 0
             for f in [0, 7]:
@@ -35,11 +35,11 @@ class Evaluador(object):
                         break
                 if mipuntuacion + tupuntuacion >= dcontar:
                     break
-            return Evaluador.esquinas_peso[banda] * (mipuntuacion - tupuntuacion)
+            return Evaluador.esquinas_valor[banda] * (mipuntuacion - tupuntuacion)
         return 0
 
     def diferencial_lados(self, dcontar, dtablero, banda):
-        if Evaluador.lados_peso[banda] != 0:
+        if Evaluador.lados_valor[banda] != 0:
             mipuntuacion = 0
             tupuntuacion = 0
             cuadrados = [(a, b) for a in [0, 7] for b in range(1, 7)] \
@@ -51,11 +51,11 @@ class Evaluador(object):
                     tupuntuacion += 1
                 if mipuntuacion + tupuntuacion >= dcontar:
                     break
-            return Evaluador.lados_peso[banda] * (mipuntuacion - tupuntuacion)
+            return Evaluador.lados_valor[banda] * (mipuntuacion - tupuntuacion)
         return 0
 
     def diferencial_x(self, tableroinicio, tableroactual, dtablero, banda):
-        if Evaluador.x_peso[banda] != 0:
+        if Evaluador.x_valor[banda] != 0:
             mipuntuacion = 0
             tupuntuacion = 0
             for x, y in [(a, b) for a in [1, 6] for b in [1, 6]]:
@@ -75,32 +75,28 @@ class Evaluador(object):
                             mipuntuacion += 1
                         elif tableroactual.tablero[x][y] == self.oponente:
                             tupuntuacion += 1
-            return Evaluador.x_peso[banda] * (mipuntuacion - tupuntuacion)
+            return Evaluador.x_valor[banda] * (mipuntuacion - tupuntuacion)
         return 0
 
-
     def diferencial_posibles(self, tableroinicio, tableroactual, banda):
-        if Evaluador.posibles_peso[banda] != 0:
+        if Evaluador.posibles_valor[banda] != 0:
             mipuntuacion = tableroactual.espaciosalcostado(
                 self.oponente) - tableroinicio.espaciosalcostado(self.oponente)
             tupuntuacion = tableroactual.espaciosalcostado(
                 self.jugador) - tableroinicio.espaciosalcostado(self.jugador)
-            return Evaluador.posibles_peso[banda] * (mipuntuacion - tupuntuacion)
+            return Evaluador.posibles_valor[banda] * (mipuntuacion - tupuntuacion)
         return 0
 
-
     def diferencial_movimientos(self, tableroinicio, tableroactual, banda):
-        mipuntuacion = len(tableroactual.movimientovalido(self.jugador)) - \
-            len(tableroinicio.movimientovalido(self.jugador))
-        tupuntuacion = len(tableroactual.movimientovalido(
-            self.oponente)) - len(tableroinicio.movimientovalido(self.oponente))
-        return Evaluador.movimientos_peso[banda] * (mipuntuacion - tupuntuacion)
+        mipuntuacion = len(tableroactual.movimientovalido(self.jugador)) - len(tableroinicio.movimientovalido(self.jugador))
+        tupuntuacion = len(tableroactual.movimientovalido(self.oponente)) - len(tableroinicio.movimientovalido(self.oponente))
+        return Evaluador.movimientos_valor[banda] * (mipuntuacion - tupuntuacion)
 
     def puntuacion(self, tableroinicio, tablero, profundidadactual, jugador, oponente):
         self.jugador = jugador
         self.oponente = oponente
-        puntuacion = 0
-        blancos, azules, vacio = tablero.contarpiezas()
+        punt = 0
+        blancos, azules, vacios = tablero.contarpiezas()
         dtablero = tablero.comparar(tableroinicio)
         dcontar = sum(dtablero.contarpiezas())
 
@@ -122,10 +118,10 @@ class Evaluador(object):
         else:
             banda = 4
 
-        puntuacion += self.diferencial_piezas(dtablero, banda)
-        puntuacion += self.diferencial_esquinas(dcontar, dtablero, banda)
-        puntuacion += self.diferencial_lados(dcontar, dtablero, banda)
-        puntuacion += self.diferencial_x(tableroinicio,tablero, dtablero, banda)
-        puntuacion += self.diferencial_posibles(tableroinicio, tablero, banda)
-        puntuacion += self.diferencial_movimientos(tableroinicio, tablero, banda)
-        return puntuacion
+        punt += self.diferencial_piezas(dtablero, banda)
+        punt += self.diferencial_esquinas(dcontar, dtablero, banda)
+        punt += self.diferencial_lados(dcontar, dtablero, banda)
+        punt += self.diferencial_x(tableroinicio,tablero, dtablero, banda)
+        punt += self.diferencial_posibles(tableroinicio, tablero, banda)
+        punt += self.diferencial_movimientos(tableroinicio, tablero, banda)
+        return punt
