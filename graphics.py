ad0f582591070e6732116428f321332dc685ede2
tamaraortizr <39891344+tamaraortizr@users.noreply.github.com>
2018-06-22 11:08:53 -0500

    Add files via upload

--- graphics.py
+++ graphics.py
@@ -1,11 +1,3 @@
-import pygame
-import sys
-from pygame.locals import *
-import time
-from variables import azul, blanco, dificultad, humano, computadora
-import os
-
-
 class layout:
     def iniciar(self):
         self.tablero = tablero.Tablero()
