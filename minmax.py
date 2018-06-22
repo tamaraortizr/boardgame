9d7987f429e0919078f3aac00c856f216bf0dbd4
tamaraortizr <39891344+tamaraortizr@users.noreply.github.com>
2018-06-22 13:32:05 -0500

    Update minmax.py

--- minmax.py
+++ minmax.py
@@ -3,25 +3,20 @@ infinito = 100000
 
 class Minimax(object):
 
-
     def __init__(self, heuristic_eval):
         self.heuristic_eval = heuristic_eval
 
-
-    def minimax(self, tablero, tableromaster, profundidad, jugador, oponente,
-                alfa = -infinito, beta = infinito):
-        bestChild = tablero
+    def minimax(self, tablero, tableromaster, profundidad, jugador, oponente, alfa = -infinito, beta = infinito):
+        bestoption = tablero
         if profundidad == 0:
-            return (self.heuristic_eval(tablero, tablero, profundidad,
-                                        jugador, oponente), tablero)
-        for child in tablero.next_states(jugador):
-            puntuacion, newChild = self.minimax(
-                child, tablero, profundidad - 1, oponente, jugador, -beta, -alfa)
+            return self.heuristic_eval(tablero, tablero, profundidad, jugador, oponente), tablero
+        for option in tablero.next_states(jugador):
+            puntuacion, newoption = self.minimax(
+                option, tablero, profundidad - 1, oponente, jugador, -beta, -alfa)
             puntuacion = -puntuacion
             if puntuacion > alfa:
                 alfa = puntuacion
-                bestChild = child
+                bestoption = option
             if beta <= alfa:
                 break
-        return (self.heuristic_eval(tablero, tablero, profundidad, jugador,
-                                    oponente), bestChild)
+        return self.heuristic_eval(tablero, tablero, profundidad, jugador, oponente), bestoption
